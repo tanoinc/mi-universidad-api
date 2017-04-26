db60ca2df6d5baa9b77c89898b8c763ca70c599b
Luciano Coggiola <lucianoc@cespi.unlp.edu.ar>
2017-04-26 15:31:21 -0300

    Nombre de aplicacion y contexto en newsfeed

--- app/Newsfeed.php
+++ app/Newsfeed.php
@@ -46,25 +46,38 @@ class Newsfeed extends Model
 
     public static function getAllFromUser($user)
     {
-        $query = DB::table('newsfeed')->whereIn('id', function($query_in) use ($user) {
+        $query = DB::table('newsfeed')
+            ->select('newsfeed.*','application.description AS application_description', 'context.name as context_name','context.description as context_description')
+            ->leftJoin('application', 'newsfeed.application_id', '=', 'application.id')
+            ->leftJoin('context', 'newsfeed.context_id', '=', 'context.id')
+            ->whereIn('newsfeed.id', function($query_in) use ($user) {
             $query_in->select('newsfeed_id')
                     ->from('newsfeed_user')
                     ->where('user_id', '=', $user->id);
-        });
-        $query2 = DB::table('newsfeed')->
-                where('global', '=', true)->
-                whereIn('application_id', function($query_in) use ($user) {
-                    $query_in->select('application_id')
-                    ->from('user_application')
-                    ->where('user_id', '=', $user->id);
-                })
-                ->union($query);
-        $query3 = DB::table('newsfeed')->
-                whereIn('context_id', function($query_in) use ($user) {
-            $query_in->select('context_id')
-            ->from('context_user_subscription')
-            ->where('user_id', '=', $user->id);
-        });
+        })
+        ->whereNull('newsfeed.deleted_at');
+        $query2 = DB::table('newsfeed')
+            ->select('newsfeed.*','application.description AS application_description', 'context.name as context_name','context.description as context_description')
+            ->where('global', '=', true)
+            ->join('application', 'newsfeed.application_id', '=', 'application.id')
+            ->leftJoin('context', 'newsfeed.context_id', '=', 'context.id')
+            ->whereIn('newsfeed.application_id', function($query_in) use ($user) {
+                $query_in->select('application_id')
+                ->from('user_application')
+                ->where('user_id', '=', $user->id);
+            })
+        ->whereNull('newsfeed.deleted_at')
+        ->union($query);
+        $query3 = DB::table('newsfeed')
+            ->select('newsfeed.*','application.description AS application_description', 'context.name as context_name','context.description as context_description')
+            ->leftJoin('application', 'newsfeed.application_id', '=', 'application.id')
+            ->leftJoin('context', 'newsfeed.context_id', '=', 'context.id')
+            ->whereIn('context_id', function($query_in) use ($user) {
+                $query_in->select('context_id')
+                ->from('context_user_subscription')
+                ->where('user_id', '=', $user->id);
+        })
+        ->whereNull('newsfeed.deleted_at');
         $query2->union($query3);
 
         return $query2;
